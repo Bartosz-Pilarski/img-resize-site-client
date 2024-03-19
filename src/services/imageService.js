04dcb8d7011bacade73b23bf3845cde0daec9eaa
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-03-19 21:06:55 +0100

    fixed commented out code

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -3,9 +3,8 @@ import axios from "axios"
 const baseUrl = 'http://localhost:3001'
 
 const submitImage = async (formData) => {
-  console.log(formData.get('extension'))
-  //const response = await axios.post(`${baseUrl}/api/images`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
-  //return `${baseUrl}/${response.data.url}`
+  const response = await axios.post(`${baseUrl}/api/images`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+  return `${baseUrl}/${response.data.url}`
 }
 
 export {
