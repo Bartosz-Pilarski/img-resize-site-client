d8f37053036e97367ed6313a8befbb0efe47ed46
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-03-20 21:39:16 +0100

    adjusted for static

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -5,7 +5,7 @@ const baseUrl = '/api/images'
 const submitImage = async (formData) => {
   try {
     const response = await axios.post(baseUrl, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
-    return { isError: false, url: `http://localhost:3001/${response.data.url}` }
+    return { isError: false, url: `/${response.data.url}` }
   } catch (error) {
     return { isError: true, status: error.response.status }
   }
